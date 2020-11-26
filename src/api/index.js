a819847defe03daf8f5d108b91b14c8fc4fc643e
shumbaashley <ashleytshumba@gmail.com>
2020-11-26 19:04:38 +0200

    setup contact form to send data

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,33 @@ import axios from 'axios'
 const ipUrl = 'http://api.ipstack.com/check?access_key=ec5d8978eca8789988fa0966bc1a23cd'
 
 export const getLocationFromIP = async () => {
-    const {data} = await axios.get(ipUrl);
-    return data;
+    try {
+        const {data} = await axios.get(ipUrl);
+        return data;
+    } catch (error) {
+        console.log(error.message)                
+    }
 }
 
 export const getCityWeatherData = async (city) => {
-    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1c170ed572096092d301640dcdf63c3f`) 
-    return data
+    try {
+        const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1c170ed572096092d301640dcdf63c3f`) 
+        return data
+    } catch (error) {
+        console.log(error.message)        
+    }
+}
+
+export const sendContactForm = async (formData) => {
+    try {
+        const config = {
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        }
+        const {data} = await axios.post('http://localhost:5000/send', formData, config)
+        return data.message
+    } catch (error) {
+        console.log(error.message)
+    }
 }
--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {sendContactForm} from '../../api'
 
 const ContactForm = () => {
-    return (
+
+	const [formData, setFormData] = useState({
+		name : '',
+		email : '',
+		company : '',
+		subject : '',
+		message : ''
+	})
+
+	const onChange = (e) => {
+		setFormData({...formData, [e.target.name] : e.target.value})
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		sendContactForm(formData)
+	}
+
+	return (
         <div className="col-md-6 col-md-offset-1">
 			<h2 className="section-title">Contact us</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consectetur inventore ducimus, facilis, numquam id soluta omnis eius recusandae nesciunt vero repellat harum cum. Nisi facilis odit hic, ipsum sed!</p>
-			<form className="contact-form">
+			<p>Please feel free to contact the persons responsible for this website via email by filling out your details on the contact form below. We will get back to you at the earliest convenience.</p>
+			<form className="contact-form" onSubmit={e => onSubmit(e)}>
 				<div className="row">
-					<div className="col-md-6"><input type="text" placeholder="Your name..."/></div>
-					<div className="col-md-6"><input type="text" placeholder="Email Addresss..."/></div>
+					<div className="col-md-6"><input name="name" type="text" placeholder="Your name..." onChange={e => onChange(e)} /></div>
+					<div className="col-md-6"><input name="email" type="text" placeholder="Email Addresss..." onChange={e => onChange(e)}/></div>
 				</div>
 			    <div className="row">
-				    <div className="col-md-6"><input type="text" placeholder="Company name..."/></div>
-						<div className="col-md-6"><input type="text" placeholder="Website..."/></div>
+						<div className="col-md-6"><input type="text" name="subject" placeholder="Subject..." onChange={e => onChange(e)}/></div>
+					    <div className="col-md-6"><input type="text" name="company" placeholder="Company name..." onChange={e => onChange(e)}/></div>
 						</div>
-						<textarea name="" placeholder="Message..."></textarea>
+						<textarea name="message" placeholder="Message..." onChange={e => onChange(e)}></textarea>
 						<div className="text-right">
 							<input type="submit" placeholder="Send message"/>
 						</div>
